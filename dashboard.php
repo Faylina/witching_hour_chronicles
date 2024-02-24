7121aeb242dbef84872446f9b7711ee48b186a29
Faylina <141265381+Faylina@users.noreply.github.com>
2024-02-24 20:20:20 +0100

    feat: Prepare the webpages for content
    A HTML header was added to the pages along with a favicon.
    
    The relevant PHP and CSS files were linked.

---
+++ dashboard.php
@@ -0,0 +1,32 @@
+<?php
+#*************************************************************************#
+
+				
+				#****************************************#
+				#********** PAGE CONFIGURATION **********#
+				#****************************************#
+
+				require_once('./include/config.inc.php');
+                require_once('./include/form.inc.php');
+                require_once('./include/db.inc.php');
+				
+				
+
+#*************************************************************************#
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="icon" type="image/x-icon" href="./css/images/favicon.ico">
+        <title>Witching Hour Chronicles - Homepage</title>
+        <link rel="stylesheet" href="./css/main.css">
+		<link rel="stylesheet" href="./css/debug.css">
+    </head>
+    <body>
+    
+    </body>
+</html>
